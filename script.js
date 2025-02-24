df2aba77dab91ff7e17beaf0b86a76d7c9078973
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-02-24 23:20:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+        document.getElementById("enterBtn").addEventListener("click", function() {
+            document.getElementById("status").outerHTML = "<h1 id='status'>Entered Metaverse</h1>";
+        });
